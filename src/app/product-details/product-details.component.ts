970c28d30bf7dd69e61c43cc4d48865ad1641e39
Serotonin7 <fliboijay@yahoo.com>
2020-04-03 16:34:02 -0400

    Cart added

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 
 import {products} from '../products';
+import {CartService} from '../cart.service';
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -13,6 +14,7 @@ product;
 
   constructor(
     private route: ActivatedRoute,
+    private cartService: CartService
   ) { }
 
   ngOnInit() {
@@ -21,4 +23,8 @@ product;
     });
   }
 
+  addToCart(product) {
+    this.cartService.addToCart(product);
+    window.alert("Your product has been added to the cart!")
+  }
 }
